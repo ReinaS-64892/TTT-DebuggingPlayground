62af2b1e78c37d7e8ad354cdcb71feaf3d3e59d1
Reina_Sakiria <sasiki.64892@gmail.com>
2024-04-01 20:54:45 +0900

    chore: create move tile test

---
+++ TTT/MoveTileTest.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+using net.rs64.TexTransTool;
+namespace net.rs64.TTTDebuggingPlayground.TTT
+{
+
+    public class MoveTileTest : MonoBehaviour
+    {
+        public Mesh Target;
+
+        [ContextMenu("Move!!!")]
+        void move()
+        {
+            var newMesh = Instantiate(Target);
+
+            var uv = newMesh.uv;
+            for (var i = 0; uv.Length > i; i += 1)
+            {
+                uv[i] += Vector2.one;
+            }
+            newMesh.uv = uv;
+
+            AssetSaveHelper.SaveAsset(newMesh);
+        }
+    }
+
+}
+#endif
