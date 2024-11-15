2b1988e74d1608df1a4db5922e75c0927c8ab569
Reina_Sakiria <rs@rs64.net>
2024-11-16 01:22:41 +0900

    chore: move to TTT

--- Editor/TTCEWgpuTest.cs
+++ Editor/TTCEWgpuTest.cs
@@ -23,170 +23,21 @@ namespace net.rs64.TexTransTool.DebuggingPlayground
         [ContextMenu("Test!!!")]
         void test()
         {
+            if (Result != null) { DestroyImmediate(Result); }
             using var ttceWgpuDevice = new TTCEWgpuDevice();
+            ttceWgpuDevice.SetDefaultTextureFormat(TexTransCoreTextureFormat.Byte);
             var sd = ShaderFinder.RegisterShaders(ttceWgpuDevice);
 
-            using var ttceWgpu = ttceWgpuDevice.GetContext<TTCEWgpuWithTTT4U>();
+            using var ttceWgpu = ttceWgpuDevice.GetContext<TTCEWgpuWithTTT4Unity>();
             ttceWgpu.ShaderDictionary = sd;
 
-
-            var resultRt = Canvas.EvaluateCanvas(ttceWgpu, 1024, 1024);
-            var tex = new Texture2D(1024, 1024, TextureFormat.RGBA32, false);
+            var resultRt = Canvas.EvaluateCanvas(ttceWgpu, Canvas?.tttImportedCanvasDescription?.Width ?? 1024, Canvas?.tttImportedCanvasDescription?.Height ?? 1024);
+            var tex = new Texture2D(resultRt.Width, resultRt.Hight, TextureFormat.RGBA32, false);
             var map = tex.GetRawTextureData<byte>();
 
             ttceWgpu.DownloadTexture(map.AsSpan(), TexTransCoreTextureFormat.Byte, resultRt);
             tex.Apply();
             Result = tex;
-        }
-    }
-
-
-    class TTCEWgpuWithTTT4U : TTCEWgpu, ITexTransToolForUnity
-    {
-        public ShaderFinder.ShaderDictionary ShaderDictionary = null!;//気を付けるようにね！
-        public ITexTransStandardComputeKey StandardComputeKey => ShaderDictionary;
-
-        public ITexTransComputeKeyDictionary<string> GrabBlend => ShaderDictionary;
-
-        public ITexTransComputeKeyDictionary<ITTBlendKey> BlendKey => ShaderDictionary;
-
-        public ITexTransComputeKeyDictionary<string> GenealCompute => ShaderDictionary.GenealCompute;
-
-        public ITTBlendKey QueryBlendKey(string blendKeyName) => ShaderDictionary.QueryBlendKey(blendKeyName);
-
-
-        public void UploadTexture<T>(ITTRenderTexture uploadTarget, ReadOnlySpan<T> bytes, TexTransCoreTextureFormat format) where T : unmanaged
-        {
-            base.UploadTexture((TTRenderTexture)uploadTarget, bytes, format);
-        }
-        public void DownloadTexture<T>(Span<T> dataDist, TexTransCoreTextureFormat format, ITTRenderTexture renderTexture) where T : unmanaged
-        {
-            base.DownloadTexture(dataDist, format, (TTRenderTexture)renderTexture);
-        }
-
-        public void LoadTexture(ITTRenderTexture writeTarget, ITTDiskTexture diskTexture)
-        {
-            if (writeTarget.Width != diskTexture.Width || writeTarget.Hight != diskTexture.Hight) { throw new ArgumentException(); }
-            var rtDisk = (RenderTextureAsDiskTexture)diskTexture;
-            CopyRenderTexture(writeTarget, rtDisk.TTRenderTexture);
-        }
-    }
-
-
-    public static class ShaderFinder
-    {
-        public static ShaderDictionary RegisterShaders(this TTCEWgpuDevice device)
-        {
-            var shaderDicts = new Dictionary<TTComputeType, Dictionary<string, TTComputeShaderID>>();
-            var ttcompPaths = GetAllShaderPathWithCurrentDirectory().ToArray();
-            foreach (var path in ttcompPaths)
-            {
-                var computeName = Path.GetFileNameWithoutExtension(path);
-
-                var srcText = File.ReadAllText(path);
-                if (srcText.Contains("UnityCG.cginc")) { throw new InvalidDataException(" UnityCG.cginc は使用してはいけません！"); }
-
-                var descriptions = TTComputeShaderUtility.Parse(srcText);
-                if (descriptions is null) { continue; }
-
-                if (shaderDicts.ContainsKey(descriptions.ComputeType) is false) { shaderDicts[descriptions.ComputeType] = new(); }
-
-                switch (descriptions.ComputeType)
-                {
-                    case TTComputeType.General:
-                        {
-                            shaderDicts[descriptions.ComputeType][computeName] = device.RegisterComputeShaderFromHLSL(path);
-                            break;
-                        }
-                    case TTComputeType.GrabBlend:
-                        {
-                            shaderDicts[descriptions.ComputeType][computeName] = device.RegisterComputeShaderFromHLSL(path);
-                            break;
-                        }
-                    case TTComputeType.Blending:
-                        {
-                            var blendKey = descriptions["Key"];
-                            var csCode = TexTransCoreEngineForUnity.TTBlendingComputeShader.KernelDefine + srcText + TTComputeShaderUtility.BlendingShaderTemplate;
-                            shaderDicts[descriptions.ComputeType][blendKey] = device.RegisterComputeShaderFromHLSL(path, csCode);
-
-                            break;
-                        }
-                }
-            }
-
-            return new(shaderDicts);
-        }
-
-        public static IEnumerable<string> GetAllShaderPathWithCurrentDirectory()
-        {
-            return Directory.GetFiles(Directory.GetCurrentDirectory(), "*.ttcomp", SearchOption.AllDirectories).Concat(Directory.GetFiles(Directory.GetCurrentDirectory(), "*.ttblend", SearchOption.AllDirectories));
-        }
-
-        public class ShaderDictionary : ITexTransStandardComputeKey, ITexTransComputeKeyDictionary<string>, ITexTransComputeKeyDictionary<ITTBlendKey>
-        {
-            private Dictionary<TTComputeType, Dictionary<string, TTComputeShaderID>> _shaderDict;
-
-
-            public ITTComputeKey this[string key] => _shaderDict[TTComputeType.GrabBlend][key];
-
-            public ITTComputeKey this[ITTBlendKey key] => ((BlendKey)key).ComputeKey;
-
-            public ITTComputeKey AlphaFill { get; private set; }
-            public ITTComputeKey AlphaCopy { get; private set; }
-            public ITTComputeKey AlphaMultiply { get; private set; }
-            public ITTComputeKey AlphaMultiplyWithTexture { get; private set; }
-            public ITTComputeKey ColorFill { get; private set; }
-            public ITTComputeKey ColorMultiply { get; private set; }
-            public ITTComputeKey BilinearReScaling { get; private set; }
-            public ITTComputeKey GammaToLinear { get; private set; }
-            public ITTComputeKey LinearToGamma { get; private set; }
-
-            public ITTComputeKey Swizzling { get; private set; }
-
-            public ITTBlendKey QueryBlendKey(string blendKeyName)
-            {
-                return new BlendKey(_shaderDict[TTComputeType.Blending][blendKeyName]);
-            }
-
-            public ITexTransComputeKeyDictionary<string> GenealCompute { get; private set; }
-
-            public ShaderDictionary(Dictionary<TTComputeType, Dictionary<string, TTComputeShaderID>> dict)
-            {
-                _shaderDict = dict;
-                AlphaFill = _shaderDict[TTComputeType.General][nameof(AlphaFill)];
-                AlphaCopy = _shaderDict[TTComputeType.General][nameof(AlphaCopy)];
-                AlphaMultiply = _shaderDict[TTComputeType.General][nameof(AlphaMultiply)];
-                AlphaMultiplyWithTexture = _shaderDict[TTComputeType.General][nameof(AlphaMultiplyWithTexture)];
-                ColorFill = _shaderDict[TTComputeType.General][nameof(ColorFill)];
-                ColorMultiply = _shaderDict[TTComputeType.General][nameof(ColorMultiply)];
-                BilinearReScaling = _shaderDict[TTComputeType.General][nameof(BilinearReScaling)];
-                GammaToLinear = _shaderDict[TTComputeType.General][nameof(GammaToLinear)];
-                LinearToGamma = _shaderDict[TTComputeType.General][nameof(LinearToGamma)];
-                Swizzling = _shaderDict[TTComputeType.General][nameof(Swizzling)];
-                GenealCompute = new GeneralComputeObject(_shaderDict[TTComputeType.General]);
-            }
-
-            class GeneralComputeObject : ITexTransComputeKeyDictionary<string>
-            {
-                private Dictionary<string, TTComputeShaderID> dictionary;
-
-                public GeneralComputeObject(Dictionary<string, TTComputeShaderID> dictionary)
-                {
-                    this.dictionary = dictionary;
-                }
-
-                public ITTComputeKey this[string key] => dictionary[key];
-            }
-        }
-
-        class BlendKey : ITTBlendKey
-        {
-            public ITTComputeKey ComputeKey;
-
-            public BlendKey(ITTComputeKey computeKey)
-            {
-                ComputeKey = computeKey;
-            }
         }
     }
 }
